7bc7e3ddec2cb8dbd4514837616f31bd7822bf62
Sandip <srai68141@gmail.com>
2023-05-23 18:27:28 +0545

    finished the crud section

--- myapp/urls.py
+++ myapp/urls.py
@@ -6,7 +6,9 @@ from . import views
 app_name = 'myapp'
 urlpatterns = [
     path('', views.index),
-    path('product/', views.products),
+    path('product/', views.products, name='product'),
     path('product/<int:id>/', views.product_detail, name='product_detail'),
     path('product/add/', views.add_product, name='add_product'),
+    path('product/edit/<int:id>/', views.update_product, name='update_product'),
+    path('product/delete/<int:id>/', views.delete_product, name='delete_product')
 ]
--- myapp/views.py
+++ myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from urllib import request
+
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Products
 # Create your views here.
@@ -30,4 +32,28 @@ def add_product(request):
        product = Products(name=name, price=price, desc=description,image=image)
        product.save()
     return render(request, 'myapp/addproduct.html')
+
+def update_product(request, id):
+    product = Products.objects.get(id=id)
+    if request.method == 'POST':
+        product.name = request.POST.get('name')
+        product.price = request.POST.get('price')
+        product.desc = request.POST.get('desc')
+        product.image = request.FILES['upload']
+        product.save()
+        return redirect('/myapp/product')
+    context = {
+        'product': product,
+    }
+    return render(request, 'myapp/updateproduct.html', context)
+
+def delete_product(request, id):
+    product = Products.objects.get(id=id)
+    context = {
+        'product': product
+    }
+    if request.method == 'POST':
+        product.delete()
+        return redirect('/myapp/product')
+    return render(request, 'myapp/delete.html', context)
 
